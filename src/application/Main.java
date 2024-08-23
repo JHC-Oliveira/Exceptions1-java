efa20d4f0017e172924b1626f327cdb71485b955
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2024-08-23 23:18:41 +0100

    Bad solution

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         Scanner kb = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+        
         System.out.print("Room number: ");
         int roomNumber = kb.nextInt();
         System.out.print("Check-in date (DD/MM/YYYY): ");
@@ -34,18 +34,13 @@ public class Main {
             System.out.print("Check-out date (DD/MM/YYYY): ");
             checkOut = sdf.parse(kb.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else if (!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null){
+                System.out.println("Error in reservation: " + error);
             }
             else {
-                reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
         }
-
     }
 }
